d04a3d28b6835d2dd90e141b7775e2bf465bcee6
June Hyok Cho <jcho0627@gmail.com>
2020-02-12 22:10:57 +0100

    initial component lecture and add MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import Menu from "./components/MenuComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"></img>
-        <navbar dark color="primary">
-          <div className="container">
-            <navBrand href="/">Ristorante Con Fusion</navBrand>
-          </div>
-        </navbar>
+          <img src={logo} className="App-logo" alt="logo"></img>
+          <navbar dark color="primary">
+            <div className="container">
+              <navBrand href="/">Ristorante Con Fusion</navBrand>
+            </div>
+          </navbar>
+          <Menu />
         </header>
       </div>
     );
